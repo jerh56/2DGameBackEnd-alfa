afb5e6c76309890106e3e1675a8be0ee6a806818
Juan Ramos <eramos@coppel.com>
2022-08-07 08:06:32 -0600

    Se mejoró el manejo del puerto y se agregó frase para prueba

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const port = 8000;
+
+app.set('port', process.env.PORT || 3000);
 
 app.get("/", (request, response) => {
     response.send("2D Video Game");
@@ -9,6 +12,10 @@ app.get("/phrase", (request, response) => {
     response.send("Star Wars Una nueva esperanza");
 });
 
-app.listen(3000, () => {
-    console.log("Listen on the port 3000...");
+app.get("/test-phrase", (request, response) => {
+    response.send("LA AMENAZA FANTASMA");
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`Server listening on port:${app.get('port')}`);
 });
