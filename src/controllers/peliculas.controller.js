95a1b30d5b5ed1774c1d3edf036343ca5b55db36
Juan Manuel Herran Calderon <juan.herran@coppel.com>
2022-09-03 15:49:46 -0600

    se adapta endpoint para que regrese la estructura de pharse

--- src/controllers/peliculas.controller.js
+++ src/controllers/peliculas.controller.js
@@ -5,12 +5,18 @@ const cPeliculas={};
 /* Se puede filtrar por:
 Id, title, overview, lenguague (en,es), page */
 cPeliculas.fncPeliculaListar=async (req,res)=>{
-    const pelicula = await mPeliculas.find(req.query)
+    const filtrado={ page: Math.floor(Math.random() * 1000), lenguague: 'es' }
+    const pelicula = await mPeliculas.find(filtrado)
     .lean();
     let result={};
     result.data=pelicula;
     result.rowCount=pelicula.length;
-    res.send(result);
+    const indice= Math.floor(Math.random() * 20);
+    res.json({
+        phrase: result.data[indice].title,
+        type: result.data[indice].media_type,
+        tries: 5
+    });
 }
 
 export default cPeliculas;
